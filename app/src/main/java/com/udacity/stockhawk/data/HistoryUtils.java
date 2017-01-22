01ac10e33c52e273f044600057fcc971052a62cd
Vincenzo Cerminara <vincenzo.cerminara94@gmail.com>
2017-01-22 18:25:49 +0100

    parsing history with HistoryUtils in DetailsActivity

---
+++ app/src/main/java/com/udacity/stockhawk/data/HistoryUtils.java
@@ -0,0 +1,37 @@
+package com.udacity.stockhawk.data;
+
+import android.support.v4.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by vinsce on 22/01/17 at 18.14.
+ *
+ * @author vinsce
+ */
+
+public final class HistoryUtils {
+    private HistoryUtils() {
+    }
+
+    public static List<Pair<Date, Double>> parseHistoryFromString(String historyString) {
+        List<Pair<Date, Double>> result = new ArrayList<>(200);
+        Scanner sc = new Scanner(historyString);
+        sc.useDelimiter("\n");
+        String tmp, dateString, valueString;
+        Date date;
+        Double value;
+        while (sc.hasNext()) {
+            tmp = sc.next();
+            dateString = tmp.substring(0, tmp.indexOf(','));
+            valueString = tmp.substring(tmp.indexOf(',') + 1);
+            date = new Date(Long.parseLong(dateString));
+            value = Double.parseDouble(valueString);
+            result.add(new Pair<>(date, value));
+        }
+        return result;
+    }
+}
--- app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.HistoryUtils;
+
+import java.util.Date;
+import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final int ID_DETAIL_LOADER = 1657;
@@ -61,6 +66,10 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         String name = cursor.getString(Contract.Quote.POSITION_NAME);
         Log.d(DetailsActivity.class.getSimpleName(), symbol);
         setTitle(name);
+
+        String historyString = cursor.getString(Contract.Quote.POSITION_HISTORY);
+        List<Pair<Date, Double>> history = HistoryUtils.parseHistoryFromString(historyString);
+        Log.d(DetailsActivity.class.getSimpleName(), historyString);
     }
 
     @Override
