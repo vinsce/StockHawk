b68eb81984d57d039a9ce1ab74f76e204e6d1236
Vincenzo Cerminara <vincenzo.cerminara94@gmail.com>
2017-01-22 17:10:30 +0100

    created (empty) details activity and starting it when clicking on a stock in main activity

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
@@ -0,0 +1,65 @@
+package com.udacity.stockhawk.ui;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+
+public class DetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int ID_DETAIL_LOADER = 1657;
+
+    private LineChart mLineChart;
+    private String mSymbol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        mSymbol = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        if (mSymbol == null) throw new NullPointerException("Symbol for DetailActivity cannot be null");
+
+        mLineChart = (LineChart) findViewById(R.id.chart);
+        getSupportLoaderManager().initLoader(ID_DETAIL_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case ID_DETAIL_LOADER:
+                Uri uri = Contract.Quote.URI.buildUpon().appendPath(mSymbol).build();
+                return new CursorLoader(this, uri, null, null, null, null);
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        boolean cursorHasValidData = false;
+        if (cursor != null && cursor.moveToFirst()) {
+            cursorHasValidData = true;
+        }
+
+        if (!cursorHasValidData) {
+            return;
+        }
+
+        String symbol = cursor.getString(Contract.Quote.POSITION_SYMBOL);
+        Log.d(DetailsActivity.class.getSimpleName(), symbol);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
